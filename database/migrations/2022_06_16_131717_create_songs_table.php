b67868d892a60d24d27dd613c14dd757d84254dc
memoray <102960499+memoray@users.noreply.github.com>
2022-06-16 15:29:51 +0200

    migrated tables

---
+++ database/migrations/2022_06_16_131717_create_songs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('artist_id');
+            $table->string('title');
+            $table->string('filename', 100);
+            $table->unsignedInteger('length');
+            $table->string('image', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
