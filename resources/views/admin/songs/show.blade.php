e1c7fe7c8239d30920ceae6e6100037a766a0c8e
memoray <102960499+memoray@users.noreply.github.com>
2022-06-29 19:32:15 +0200

    assigned css classes for audio player

--- resources/views/admin/songs/show.blade.php
+++ resources/views/admin/songs/show.blade.php
@@ -1,27 +1,30 @@
 @extends('layouts.app')
+@push('styles')
+    <link href="{{ asset('css/songsShow.css') }}" rel="stylesheet">
+@endpush
 @section('content')
 
-    <div class="container">
-        <h1>Play Songs</h1>
-        <table class="songTitle d-flex justify-content-between">
-            <tr>
-                <th >Artist: </th>
-                <td>{{ $song->artist->name}}</td>
-            </tr>
-            <tr>
-                <th>Song Title: </th>
-                <td>{{ $song->title}}</td>
-            </tr>
-        </table>
-        <div class="player">
-            <img class="img" src="/storage/albumImages/{{ $song->image }}" height="300">
+<div class="player-container">
+    <div class="c-player">
+        <h1>Play Song</h1>
+
+        <div class="c-player--details">
+            <div class="details-img">
+                <img src="/storage/albumImages/{{ $song->image }}" height="300" alt="" />
+            </div>
+            <h3 class="details-title">{{ $song->title}}</h3>
+            <h4 class="details-artist">{{ $song->artist->name}}</h4>
         </div>
-        <div>
+        <div class="controls" >
             <audio controls>
-                  <source src="/storage/songs/{{ $song->filename }}">
+                <source src="/storage/songs/{{ $song->filename }}">
             </audio>
         </div>
+
     </div>
+</div>
+
 
 
 @endsection
+
