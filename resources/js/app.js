2c9d03dd00fcb7964a89f639be9a37a8c1d8722f
memoray <102960499+memoray@users.noreply.github.com>
2022-06-29 11:22:56 +0200

    created fade out for flash-message

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,4 +3,22 @@ import './bootstrap';
 window.onload = () => {
     document.querySelectorAll('.delsoft')
          .forEach(item => item.onclick = () => confirm('Daten wirklich löschen?'));
+
+
+    const flashMsg = document.querySelector('h3.alert');
+    if(flashMsg) {
+        const btnClose = flashMsg.querySelector('span .bi-x-circle-fill');
+        btnClose.onclick = () => {
+            flashMsg.style.display = "none";
+        }
+        const to = setTimeout(() => {
+            flashMsg.classList.toggle('fadeout');
+            const to1 = setTimeout(() => {
+                flashMsg.style.display = "none";
+                clearTimeout(to)
+                clearTimeout(to1)
+            }, 3000);
+        }, 3000);
+    }
 }
+
--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,5 @@
 @if(session('success'))
-    <h3 class="alert alert-success">{{ session('success') }}</h3>
+    <h3 class="container alert alert-success">{{ session('success') }}<span class="float-end"><i class="bi bi-x-circle-fill"></i></span></h3>
 @elseif(session('error'))
-    <h3 class="alert alert-danger">{{ session('error') }}</h3>
+    <h3 class="container alert alert-danger">{{ session('error') }}<span class="float-end"><i class="bi bi-x-circle-fill"></i></span></h3>
 @endif
